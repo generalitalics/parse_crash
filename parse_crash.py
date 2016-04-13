1cd2f2d54c4162fbe436b41655a9671d4a1ac125
generalitalics <skamus00love88>
2016-04-13 17:09:26 +0300

    1

--- parse_crash.py
+++ parse_crash.py
@@ -22,6 +22,7 @@ print 'res_dir = '+res_dir
 if not os.path.exists(res_dir):
     os.makedirs(res_dir)
 # makedirs - создает папку по директории с результатами, если ее нету
+# result - ключ + значение
 result = {}
 #line - это строка во всем lines
 for line in lines:
@@ -31,11 +32,10 @@ for line in lines:
     print key
     # с помощью регулярных выражений убираем лишнее
     key = re.sub(r"\d+-\d", "", key)
-    print key
     key = re.sub(r"\d+$", "", key)
-    print key
     if result.get(key, False) is False:
         result[key] = []
+    print result.get(key, False)
     result[key].append("|".join(l))
 
 for k, s in result.iteritems():
@@ -43,6 +43,7 @@ for k, s in result.iteritems():
     with open(os.path.join(".", res_dir, k + ".csv"), "w") as w:
         w.write(text)
 
+# сколько выполнялась это чудесная програмуля
 time_end = datetime.now()
 time_delta = time_start - time_end
 print "Total time: %d s" % -time_delta.total_seconds()
